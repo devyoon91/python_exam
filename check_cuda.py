a73ae810cb3afc3d52378f3f92739e09638ff7d4
김병윤 <kimbyungyoun91@gmail.com>
2023-03-02 14:48:47 +0900

    fix: 오타수정

--- check_cuda.py
+++ check_cuda.py
@@ -3,7 +3,7 @@ import torch
 # pip install torch
 
 if torch.cuda.is_available() is False:
-    print("CUDA is not impossible")
+    print("CUDA is impossible")
 else:
     print("현재 디바이스 :{}".format(torch.cuda.current_device()))
     print("디바이스 갯수 :{}".format(torch.cuda.device_count()))
